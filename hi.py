0662cda122e988b210eb564cbd3fd5e37ddd2504
hoseung1102 <ghtmd00@snu.ac.kr>
2026-01-16 01:36:55 +0900

    fix:for loop

--- hi.py
+++ hi.py
@@ -4,10 +4,15 @@ successful = False
 for i in range(3):
     print("메시지 전송 시도 중")
     random_number = random.randint(1,10)
-    if random_number < 3:
+    print(f'{i+1}번 째 시도 중')
+    for j in range(3):
+        random_number += 1
+    if random_number < 5:
         successful = True
     if successful:
         break
-    else:
-        print("세 번 시도했으나 실패했습니다")
-    
+
+if successful:    
+    print("메시지 전송 성공!")
+else:
+    print(f'세 번 시도했으나 실패했습니다')
