2d8a42a1949ac81c9de9784a66993dd0956d619a
hoseung1102 <ghtmd00@snu.ac.kr>
2026-01-16 01:31:36 +0900

    feat:for loop

--- hi.py
+++ hi.py
@@ -1,2 +1,13 @@
-print("hello world")
+import random 
 
+successful = False
+for i in range(3):
+    print("메시지 전송 시도 중")
+    random_number = random.randint(1,10)
+    if random_number < 3:
+        successful = True
+    if successful:
+        break
+    else:
+        print("세 번 시도했으나 실패했습니다")
+    
